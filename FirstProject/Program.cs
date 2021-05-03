bad065c854c9991195518c5008bf50b39f3211d2
LGDSC <gvslakshman@gmail.com>
2021-05-03 14:53:27 -0700

    Adding firstProject

---
+++ FirstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //To Run your console app, run dotbet run on the folder that contains the .Net Project
+            //dotnet build,this commane make sure the your code compliles
+            //dotnet run build and runs your code
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
